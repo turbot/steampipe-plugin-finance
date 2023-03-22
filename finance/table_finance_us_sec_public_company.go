c10876f4739803ae2a8b453c5b246ede764213dc
Ved misra <47312748+misraved@users.noreply.github.com>
2023-03-23 02:05:32 +0530

    Recompile plugin with steampipe-plugin-sdk v5.3.0 (#20)

--- finance/table_finance_us_sec_public_company.go
+++ finance/table_finance_us_sec_public_company.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/piquette/edgr/core"
 
-	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
 func tableFinanceUsSecPublicCompany(ctx context.Context) *plugin.Table {
